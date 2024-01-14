ebd41b1273946ce6fa1ad87009cdbde83907d2d6
vishnuathreya28 <vishnu.athreya77@gmail.com>
2024-01-14 21:00:20 +0530

    cors added

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const todoRoutes = require('./routes/todoRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3001;
 
 //acts as the middleware parsing the incoming request bodies before we use the data
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/todo', {useNewUrlParser: true, useUnifiedTopology: true});
 
